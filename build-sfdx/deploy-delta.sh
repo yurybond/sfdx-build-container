66b2e97d1340eff06b1a4e7c6d51a3f4785a785e
Yury Bondarau <yurybondarau@gmail.com>
2018-03-21 16:26:20 +0100

    Script deploy full aura bundle directory instead of separate files/script ignores files that was deleted

--- build-sfdx/deploy-delta.sh
+++ build-sfdx/deploy-delta.sh
@@ -10,11 +10,11 @@ fi;
 echo "This is a custom shell script that check difference betwen $BRANCH_NAME branch deploy this difference to the org where you are currently authorized with Force.com CLI"
 echo "Prerequisites: Git installed, Force.com CLI installed"
 
-FILES_CHANGED=$(git diff $BRANCH_NAME --name-only src/)
+FILES_CHANGED=$(git diff $BRANCH_NAME --name-only --diff-filter=AM src/)
 LATEST_MASTER_MERGE=$(git --no-pager log --all --grep="Merged .* into ${BRANCH_NAME}" | grep -m 1 -owh "[0-9A-fa-f]*")
 
 if [[ ${FILES_CHANGED} != *"src/"* ]]; then
-	FILES_CHANGED="$(git diff $LATEST_MASTER_MERGE --name-only src/)"
+	FILES_CHANGED="$(git diff $LATEST_MASTER_MERGE --name-only --diff-filter=AM src/)"
 fi;
 
 if [[ $FILES_CHANGED ]]; then
@@ -28,7 +28,14 @@ fi;
 DEPLOY_COMMAND="force push "
 for item in ${FILES_CHANGED}
 do
-	if [[ ${item} = *"src/destructiveChanges"* ]] || [[ ${item} = *"src/package.xml"* ]]; then
+#for files from aura component folder we are going to pull full component directory
+	if [[ ${item} = *"src/aura/"* ]]; then
+		AURA_COMPONENT_DIR=$(dirname "${item}")
+		DEPLOY_COMMAND+=" -f "
+		DEPLOY_COMMAND+="\"${AURA_COMPONENT_DIR}\""
+	fi;
+	
+	if [[ ${item} = *"src/package.xml"* ]]; then
 		echo "Ignore ${item}"
 	else
 		DEPLOY_COMMAND+=" -f "
